0cb607a8c84aa05a1afa5c76e6303fd637202aba
Phung Lai <tl353@njit.edu>
2018-10-15 16:04:25 -0400

    Task: Initialize Laravel project, add About

---
+++ Laravel_Project/resources/views/pages/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+@section('content')
+
+    <h1> <br><br> Hello World Welcome to my About Page</h1>
+
+
+
+@stop
---
+++ Laravel_Project/resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.default')
+@section('content')
+
+    <h1> <br> <br>Hello World Welcome to my Homepage</h1>
+
+
+
+@stop
